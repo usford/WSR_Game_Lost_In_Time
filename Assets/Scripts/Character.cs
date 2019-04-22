a2392fa625e969ca8e950e12581db193a469e9d7
Test <lukichev227@mail.ru>
2019-04-22 17:49:26 +0300

    bbb
    mmm

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -1,6 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System;
 
 public class Character : MonoBehaviour
 {
@@ -8,23 +10,58 @@ public class Character : MonoBehaviour
     private int gold = 0;
     [SerializeField]
     private float speed = 3.0f;
+    [SerializeField]
+    private float timeSpeed = 4.5f;
+    [SerializeField]
+    private float sliderSec = 4;
 
     private Rigidbody2D rigibody;
     private SpriteRenderer sprite;
+    private GameObject slider;
+
+    List<Vector3> lines = new List<Vector3>(); //Создание веторов для отмотки времени
+    List<bool> flipesX = new List<bool>(); //Считывание направление игрока для отмотки времени
 
     private bool isStairs = false; //Находится ли персонаж возле лестницы
     private bool isTrumplet = false; //Находится ли персонаж возле трубы
+
+    private float time;
+    private float time2;
+    private int indexLines = 9;
 
     private void Awake()
     {
         rigibody = GetComponent<Rigidbody2D>();
         sprite = GetComponentInChildren<SpriteRenderer>();
+        time = 0;
+        slider = GameObject.Find("Slider");
     }
-
     private void Update()
     {
         if (Input.GetButton("Horizontal")) Run();
-        if (Input.GetButton("Vertical") && isStairs == true) Climb();
+        if (Input.GetButton("Vertical") && (isStairs == true || isTrumplet == true)) Climb();
+        if (Input.GetButton("Fire1")) { BackTime(); } else { RegenirationTime(); };
+        //if (Input.GetKeyDown(KeyCode.Space)) transform.position = Vector3.MoveTowards(transform.position, lines[0], speed * Time.deltaTime);
+        time += Time.deltaTime;
+        time2 += Time.deltaTime;
+        if (time > 0.33F && !Input.GetButton("Fire1"))
+        {
+            CreateLines();
+            time = 0;
+            Debug.Log("Создали вектор");
+        }
+        //Debug.Log(time2);
+
+    }
+
+    private void CreateLines()
+    {
+        if (lines.Count == 10)
+        {
+            lines.RemoveAt(0);
+        }
+        lines.Add(new Vector3(transform.position.x, transform.position.y, transform.position.z));
+        flipesX.Add(sprite.flipX);
     }
 
     /// <summary>
@@ -37,8 +74,8 @@ public class Character : MonoBehaviour
         transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
 
         sprite.flipX = direction.x > 0.0F;
-    }
 
+    }
     /// <summary>
     /// Карабкаться вверх-вниз
     /// </summary>
@@ -46,8 +83,42 @@ public class Character : MonoBehaviour
     {
         Vector3 direction = transform.up * Input.GetAxis("Vertical");
 
-        transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+        if (isStairs == true)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+        }
+
+
+        if (isTrumplet == true && Input.GetAxis("Vertical") < 0)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, transform.position + direction, speed * Time.deltaTime);
+        }
     }
+
+    //Перемотка времени
+    private void BackTime()
+    {
+        slider.GetComponent<Slider>().value = slider.GetComponent<Slider>().value - Time.deltaTime * timeSpeed;
+
+        transform.position = Vector3.MoveTowards(transform.position, lines[indexLines], speed * Time.deltaTime);
+        sprite.flipX = flipesX[indexLines];
+        Debug.Log(transform.position + "::::::::::::::::" + lines[indexLines] + ":::::::::::::::::" + flipesX[indexLines]);
+        if (transform.position == lines[indexLines])
+        {
+            lines.RemoveAt(indexLines);
+            flipesX.RemoveAt(indexLines);
+            indexLines--;
+        }
+
+       
+    }
+
+    //Восстановление времени
+    private void RegenirationTime()
+    {
+        slider.GetComponent<Slider>().value = slider.GetComponent<Slider>().value + Time.deltaTime * timeSpeed;
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -56,14 +127,23 @@ public class Character : MonoBehaviour
             Destroy(collision.gameObject);
             gold++;
         }
+
+        if (collision.CompareTag("Stairs"))
+        {
+            rigibody.gravityScale = 0;
+        }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.tag == "Stairs")
         {
-            isStairs = true;
-            rigibody.gravityScale = 0;
+            float position = collision.transform.position.x - transform.position.x;
+            if (position <= 0.125f && position >= -0.125f)
+            {
+                isStairs = true;
+                rigibody.gravityScale = 0;
+            }
         }
 
         if (collision.tag == "Trumplet")
